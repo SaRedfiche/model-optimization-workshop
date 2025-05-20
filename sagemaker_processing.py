6a96ac55dfef489f36c1e17c29dbafc24a422fc4
Robert Fisher <fisherrn@amazon.com>
2025-05-20 15:49:29 -0700

    Implement distributed pruning and add sagemaker_processing.py utility

--- sagemaker_processing.py
+++ sagemaker_processing.py
@@ -1,256 +1,161 @@
 """
-Utility functions for running optimization tasks on separate SageMaker instances.
-This allows using a small notebook instance while offloading heavy computation to larger instances.
+Utility functions for running distributed processing jobs on SageMaker.
 """
 
-import os
-import json
 import boto3
 import sagemaker
 from sagemaker.processing import ProcessingInput, ProcessingOutput, Processor
 from sagemaker.pytorch.processing import PyTorchProcessor
 
-def run_optimization_job(
-    script_path,
-    instance_type="ml.g4dn.xlarge",
-    inputs=None,
-    outputs=None,
-    job_name=None,
-    role=None,
-    framework_version="1.13.1",
-    py_version="py39",
-    wait=True
-):
+def run_quantization_job(processor, model_key, model_info, s3_bucket, 
+                         quantization_method='dynamic', quantization_bits=8):
     """
-    Run an optimization task on a separate SageMaker instance.
+    Run a quantization job on SageMaker Processing.
     
     Args:
-        script_path: Path to the Python script that performs the optimization
-        instance_type: SageMaker instance type to use
-        inputs: Dictionary of input data sources
-        outputs: Dictionary of output destinations
-        job_name: Name for the processing job
-        role: SageMaker execution role ARN
-        framework_version: PyTorch version
-        py_version: Python version
-        wait: Whether to wait for the job to complete
+        processor: SageMaker PyTorchProcessor instance
+        model_key: Key identifying the model
+        model_info: Dictionary with model information
+        s3_bucket: S3 bucket name
+        quantization_method: Method for quantization ('dynamic', 'static', or 'aware')
+        quantization_bits: Number of bits for quantization (8 or 16)
         
     Returns:
-        SageMaker processing job object
+        SageMaker processing job
     """
-    # Get SageMaker session and role
-    sagemaker_session = sagemaker.Session()
-    if role is None:
-        role = sagemaker.get_execution_role()
+    # Define inputs and outputs
+    inputs = [
+        ProcessingInput(
+            source=f's3://{s3_bucket}/scripts/quantization_script.py',
+            destination='/opt/ml/processing/input/code/quantization_script.py'
+        ),
+        ProcessingInput(
+            source=f's3://{s3_bucket}/optimization/inputs/{model_key}/model_info.json',
+            destination='/opt/ml/processing/input/data/model_info.json'
+        )
+    ]
     
-    # Generate job name if not provided
-    if job_name is None:
-        import time
-        job_name = f"model-optimization-{int(time.time())}"
-    
-    # Create processor
-    processor = PyTorchProcessor(
-        framework_version=framework_version,
-        py_version=py_version,
-        role=role,
-        instance_type=instance_type,
-        instance_count=1,
-        base_job_name=job_name,
-        sagemaker_session=sagemaker_session
-    )
-    
-    # Prepare inputs
-    processing_inputs = []
-    if inputs:
-        for name, source in inputs.items():
-            processing_inputs.append(
-                ProcessingInput(
-                    source=source,
-                    destination=f"/opt/ml/processing/input/{name}",
-                    input_name=name
-                )
-            )
-    
-    # Prepare outputs
-    processing_outputs = []
-    if outputs:
-        for name, destination in outputs.items():
-            processing_outputs.append(
-                ProcessingOutput(
-                    source=f"/opt/ml/processing/output/{name}",
-                    destination=destination,
-                    output_name=name
-                )
-            )
+    outputs = [
+        ProcessingOutput(
+            source='/opt/ml/processing/output',
+            destination=f's3://{s3_bucket}/optimization/outputs/{model_key}'
+        )
+    ]
     
     # Run the processing job
-    processor.run(
-        code=script_path,
-        inputs=processing_inputs,
-        outputs=processing_outputs,
-        wait=wait
-    )
-    
-    return processor
-
-def run_quantization_job(
-    model_key,
-    model_info,
-    s3_bucket,
-    instance_type="ml.c5.xlarge",
-    role=None
-):
-    """
-    Run quantization on a separate SageMaker instance.
-    
-    Args:
-        model_key: Key of the model to quantize
-        model_info: Dictionary with model information
-        s3_bucket: S3 bucket for storing inputs and outputs
-        instance_type: SageMaker instance type to use
-        role: SageMaker execution role ARN
-        
-    Returns:
-        S3 path to the quantized model
-    """
-    # Save model info to a temporary file
-    with open('temp_model_info.json', 'w') as f:
-        json.dump({model_key: model_info[model_key]}, f)
-    
-    # Upload to S3
-    s3_client = boto3.client('s3')
-    s3_client.upload_file(
-        'temp_model_info.json', 
-        s3_bucket, 
-        f'optimization/inputs/{model_key}/model_info.json'
-    )
-    
-    # Define inputs and outputs
-    inputs = {
-        'model_info': f's3://{s3_bucket}/optimization/inputs/{model_key}/model_info.json'
-    }
-    
-    outputs = {
-        'quantized_model': f's3://{s3_bucket}/optimization/outputs/{model_key}/quantized'
-    }
-    
-    # Run the job
-    processor = run_optimization_job(
-        script_path='quantization_script.py',
-        instance_type=instance_type,
+    job = processor.run(
+        code='quantization_script.py',
         inputs=inputs,
         outputs=outputs,
-        job_name=f'quantize-{model_key}',
-        role=role
+        arguments=[
+            '--model-info-path', '/opt/ml/processing/input/data/model_info.json',
+            '--output-dir', '/opt/ml/processing/output',
+            '--quantization-method', quantization_method,
+            '--quantization-bits', str(quantization_bits)
+        ]
     )
     
-    # Return the S3 path to the quantized model
-    return f's3://{s3_bucket}/optimization/outputs/{model_key}/quantized'
+    return job
 
-def run_pruning_job(
-    model_key,
-    model_info,
-    s3_bucket,
-    instance_type="ml.g4dn.xlarge",
-    role=None
-):
+def run_pruning_job(processor, model_key, model_info, s3_bucket, 
+                    pruning_method='l1_unstructured', pruning_amount=0.3):
     """
-    Run pruning on a separate SageMaker instance.
+    Run a pruning job on SageMaker Processing.
     
     Args:
-        model_key: Key of the model to prune
+        processor: SageMaker PyTorchProcessor instance
+        model_key: Key identifying the model
         model_info: Dictionary with model information
-        s3_bucket: S3 bucket for storing inputs and outputs
-        instance_type: SageMaker instance type to use
-        role: SageMaker execution role ARN
+        s3_bucket: S3 bucket name
+        pruning_method: Method for pruning ('l1_unstructured', 'random_unstructured', or 'ln_structured')
+        pruning_amount: Amount of weights to prune (0.0 to 1.0)
         
     Returns:
-        S3 path to the pruned model
+        SageMaker processing job
     """
-    # Save model info to a temporary file
-    with open('temp_model_info.json', 'w') as f:
-        json.dump({model_key: model_info[model_key]}, f)
-    
-    # Upload to S3
-    s3_client = boto3.client('s3')
-    s3_client.upload_file(
-        'temp_model_info.json', 
-        s3_bucket, 
-        f'optimization/inputs/{model_key}/model_info.json'
-    )
-    
     # Define inputs and outputs
-    inputs = {
-        'model_info': f's3://{s3_bucket}/optimization/inputs/{model_key}/model_info.json'
-    }
+    inputs = [
+        ProcessingInput(
+            source=f's3://{s3_bucket}/scripts/pruning_script.py',
+            destination='/opt/ml/processing/input/code/pruning_script.py'
+        ),
+        ProcessingInput(
+            source=f's3://{s3_bucket}/optimization/inputs/{model_key}/model_info.json',
+            destination='/opt/ml/processing/input/data/model_info.json'
+        )
+    ]
     
-    outputs = {
-        'pruned_model': f's3://{s3_bucket}/optimization/outputs/{model_key}/pruned'
-    }
+    outputs = [
+        ProcessingOutput(
+            source='/opt/ml/processing/output',
+            destination=f's3://{s3_bucket}/optimization/outputs/{model_key}'
+        )
+    ]
     
-    # Run the job
-    processor = run_optimization_job(
-        script_path='pruning_script.py',
-        instance_type=instance_type,
+    # Run the processing job
+    job = processor.run(
+        code='pruning_script.py',
         inputs=inputs,
         outputs=outputs,
-        job_name=f'prune-{model_key}',
-        role=role
+        arguments=[
+            '--model-info-path', '/opt/ml/processing/input/data/model_info.json',
+            '--output-dir', '/opt/ml/processing/output',
+            '--pruning-method', pruning_method,
+            '--pruning-amount', str(pruning_amount)
+        ]
     )
     
-    # Return the S3 path to the pruned model
-    return f's3://{s3_bucket}/optimization/outputs/{model_key}/pruned'
+    return job
 
-def run_distillation_job(
-    model_key,
-    model_info,
-    s3_bucket,
-    instance_type="ml.g4dn.2xlarge",
-    role=None
-):
+def run_distillation_job(processor, model_key, teacher_model_info, s3_bucket, 
+                         student_model_name='distilbert-base-uncased', 
+                         num_epochs=3, batch_size=8):
     """
-    Run knowledge distillation on a separate SageMaker instance.
+    Run a knowledge distillation job on SageMaker Processing.
     
     Args:
-        model_key: Key of the model to distill
-        model_info: Dictionary with model information
-        s3_bucket: S3 bucket for storing inputs and outputs
-        instance_type: SageMaker instance type to use
-        role: SageMaker execution role ARN
+        processor: SageMaker PyTorchProcessor instance
+        model_key: Key identifying the model
+        teacher_model_info: Dictionary with teacher model information
+        s3_bucket: S3 bucket name
+        student_model_name: Name of the student model architecture
+        num_epochs: Number of training epochs
+        batch_size: Training batch size
         
     Returns:
-        S3 path to the distilled model
+        SageMaker processing job
     """
-    # Save model info to a temporary file
-    with open('temp_model_info.json', 'w') as f:
-        json.dump({model_key: model_info[model_key]}, f)
-    
-    # Upload to S3
-    s3_client = boto3.client('s3')
-    s3_client.upload_file(
-        'temp_model_info.json', 
-        s3_bucket, 
-        f'optimization/inputs/{model_key}/model_info.json'
-    )
-    
     # Define inputs and outputs
-    inputs = {
-        'model_info': f's3://{s3_bucket}/optimization/inputs/{model_key}/model_info.json'
-    }
+    inputs = [
+        ProcessingInput(
+            source=f's3://{s3_bucket}/scripts/distillation_script.py',
+            destination='/opt/ml/processing/input/code/distillation_script.py'
+        ),
+        ProcessingInput(
+            source=f's3://{s3_bucket}/optimization/inputs/{model_key}/model_info.json',
+            destination='/opt/ml/processing/input/data/model_info.json'
+        )
+    ]
     
-    outputs = {
-        'distilled_model': f's3://{s3_bucket}/optimization/outputs/{model_key}/distilled'
-    }
+    outputs = [
+        ProcessingOutput(
+            source='/opt/ml/processing/output',
+            destination=f's3://{s3_bucket}/optimization/outputs/{model_key}_distilled'
+        )
+    ]
     
-    # Run the job
-    processor = run_optimization_job(
-        script_path='distillation_script.py',
-        instance_type=instance_type,
+    # Run the processing job
+    job = processor.run(
+        code='distillation_script.py',
         inputs=inputs,
         outputs=outputs,
-        job_name=f'distill-{model_key}',
-        role=role
+        arguments=[
+            '--model-info-path', '/opt/ml/processing/input/data/model_info.json',
+            '--output-dir', '/opt/ml/processing/output',
+            '--student-model-name', student_model_name,
+            '--num-epochs', str(num_epochs),
+            '--batch-size', str(batch_size)
+        ]
     )
     
-    # Return the S3 path to the distilled model
-    return f's3://{s3_bucket}/optimization/outputs/{model_key}/distilled'
+    return job
